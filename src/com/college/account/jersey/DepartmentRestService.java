e1ebbb1c9b1c5b8bd5c21d6355070be9a202488f
xxx2406070412 <906702424@qq.com>
2015-05-12 01:10:12 +0800

    提出 deparment

--- src/com/college/account/jersey/DepartmentRestService.java
+++ src/com/college/account/jersey/DepartmentRestService.java
@@ -1,101 +1,124 @@
 package com.college.account.jersey;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
+
 import com.college.account.bean.Department;
+import com.college.account.service.DaoDepartmentService;
+import com.college.account.service.DaoDeptOrgService;
+import com.college.account.service.DaoOrganizationService;
 import com.college.util.Cause;
 import com.college.util.Json2Obj;
-import com.college.util.Obj2Map;
+import com.college.util.Logger4j;
 import com.college.util.ServiceFactoryBean;
 
 
-@Path("/departments")
+@Path("/organizations/{id}/departments")
 public class DepartmentRestService {
-	
-	private static int DEPLOSTNAME = 4001;
-	private static int DEPIDNOTFIND = 4002;
-	
-	private static String tablename = "Department";
 
+	private static DaoOrganizationService p = ServiceFactoryBean.getOrganizationService();
+	private static DaoDepartmentService pD = ServiceFactoryBean.getDepartmentService();
+    private static DaoDeptOrgService pDO = ServiceFactoryBean.getDeptOrgService();
+    private static final Logger log = Logger4j.getLogger(DepartmentRestService.class);
+    
+	@PUT
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String saveDep(@PathParam("id") String id,
+                                           String jsonString){
+    	try {
+			
+			if(!p.selIsExist(Integer.parseInt(id))){
+				return Cause.getFailcode(DaoOrganizationService.ORGIDNOTFIND, "id", "id not find");
+			}
+			
+			Department department = (Department)Json2Obj.getObj(jsonString, Department.class);
+			
+			if(null == department.getId()){
+				/* need save*/
+				if(null == department.getName()){
+					return Cause.getFailcode(DaoDepartmentService.DEPARTMENTLOSTNAME, "name", "must has name");
+				}else{
+					Integer resultid = null;
+					if(pD.selNameUniq(department.getName())){
+						resultid = Cause.getResultId(pD.save(jsonString));
+					}else{
+						resultid = ((Department)pD.searchByFeild(DaoDepartmentService.tablename, "name", department.getName())).getId();
+					}
+					
+					pDO.save(Integer.parseInt(id), resultid, department.getOperId());
+					
+					return Cause.getSuccess(resultid);
+				}
+			}
+			
+			if(!pD.selIsExist(department.getId())){
+				return Cause.getFailcode(DaoOrganizationService.ORGIDNOTFIND, "id", "id not find");
+			}
+			
+			pDO.save(Integer.parseInt(id), department.getId(), department.getOperId());
+			
+			return Cause.getSuccess(department.getId());
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error(e);
+		}
+		return Cause.getFailcode(3000, "", "system error");
+    }
+    
+    @DELETE
+    @Path("/{idD}")
+    public String deleteDep(@PathParam("id") String id,
+    		                @PathParam("idD") String idD){
+    	
+    	try{
+    		return pDO.del(id, idD);
+    	}catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error(e);
+		}
+		return Cause.getFailcode(3000, "", "system error");
+    	
+    }
+    
+    @GET
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String getDep(@PathParam("id") String id){
+    	
+    	try{
+    		
+    		return pDO.get(id);
+    		
+    	}catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error(e);
+		}
+		return Cause.getFailcode(3000, "", "system error");
+    }
+    
+    @GET
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Path("/{idD}")
+    public String getOneDep(@PathParam("id") String id,
+                            @PathParam("idD") String idD){
+    	try{
+    		
+    		return pDO.getOne(id, idD);
+    		
+    	}catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error(e);
+		}
+		return Cause.getFailcode(3000, "", "system error");
+    }	
 	
-	@POST
-    @Consumes(MediaType.APPLICATION_JSON)  
-	public String save(String jsonString){
-	     
-	  Integer id = null;
-      Department department = null;
-      department = (Department)Json2Obj.getObj(jsonString, Department.class);
-      
-      if(null == department.getName()){
-          return Cause.getFailcode(DEPLOSTNAME, "name", null);
-      }
-      
-      department.setCreateTime(new Date());
-      
-      id = ServiceFactoryBean.getDepartmentService().create(department);
-      
-      return Cause.getSuccess(id);
-	}
-	
-	 @GET
-	 @Consumes({MediaType.APPLICATION_JSON})
-	 @Path("/{id}")
-	 public String searchInfo(@PathParam("id") String id)
-	 {
-		 Department department = null;
-		 List list = new ArrayList(1);
-		 
-		 department = ServiceFactoryBean.getDepartmentService().searchByid(Integer.parseInt(id), tablename);
-		 
-		 if(null == department){ return Cause.getFailcode(DEPIDNOTFIND, "id", null);}
-		 
-		 list.add(Obj2Map.toMap(department, Department.class));
-		 
-		 return Cause.getData(list);
-	 }
-	 
-	 @PUT
-	 @Consumes({MediaType.APPLICATION_JSON})
-	 @Path("/{id}")
-	 public String updateInfo(@PathParam("id") String id,
-	                                           String jsonString){
-		 
-		 Department department = null;
-		 Department departmentfind = null;
-		 department = (Department)Json2Obj.getObj(jsonString, Department.class);
-		 
-		 department.setId(Integer.parseInt(id));
-		 
-		 departmentfind = ServiceFactoryBean.getDepartmentService().searchByid(department.getId(), tablename);
-		 
-		 if(null == department){ return Cause.getFailcode(DEPIDNOTFIND, "id", null);}
-		 
-		 Json2Obj.updateObject(department, departmentfind);
-		 
-		 ServiceFactoryBean.getDepartmentService().update(departmentfind);
-		 
-		 return Cause.getSuccess(null);
-	 }
-	 
-	 @DELETE
-	 @Consumes({MediaType.APPLICATION_JSON})
-	 @Path("{id}")
-	 public String deleteInfo(@PathParam("id") String id){
-		 
-		 ServiceFactoryBean.getDepartmentService().delete(Department.class, Integer.parseInt(id));
-		 
-		 return Cause.getSuccess(null);
-	 }
 
 }
--- src/com/college/account/jersey/OrganizationRestService.java
+++ src/com/college/account/jersey/OrganizationRestService.java
@@ -1,9 +1,6 @@
 package com.college.account.jersey;
 
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,16 +11,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
 
-import com.college.account.bean.Department;
-import com.college.account.bean.DeptOrg;
-import com.college.account.bean.Organization;
-import com.college.account.service.DaoDepartmentService;
-import com.college.account.service.DaoDeptOrgService;
 import com.college.account.service.DaoOrganizationService;
 import com.college.util.Cause;
-import com.college.util.Json2Obj;
+
 import com.college.util.Logger4j;
-import com.college.util.Obj2Map;
 import com.college.util.ServiceFactoryBean;
 
 
@@ -31,8 +22,7 @@ import com.college.util.ServiceFactoryBean;
 public class OrganizationRestService
 {
     private static DaoOrganizationService p = ServiceFactoryBean.getOrganizationService();
-    private static DaoDepartmentService pD = ServiceFactoryBean.getDepartmentService();
-    private static DaoDeptOrgService pDO = ServiceFactoryBean.getDeptOrgService();
+    
     
     private static final Logger log = Logger4j.getLogger(OrganizationRestService.class);
     
@@ -105,102 +95,8 @@ public class OrganizationRestService
 	}
     
     
-    @PUT
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("/{id}/departments")
-    public String saveDep(@PathParam("id") String id,
-                                           String jsonString){
-    	try {
-			
-			if(!p.selIsExist(Integer.parseInt(id))){
-				return Cause.getFailcode(DaoOrganizationService.ORGIDNOTFIND, "id", "id not find");
-			}
-			
-			Department department = (Department)Json2Obj.getObj(jsonString, Department.class);
-			
-			if(null == department.getId()){
-				/* need save*/
-				if(null == department.getName()){
-					return Cause.getFailcode(DaoDepartmentService.DEPARTMENTLOSTNAME, "name", "must has name");
-				}else{
-					Integer resultid = null;
-					if(pD.selNameUniq(department.getName())){
-						resultid = Cause.getResultId(pD.save(jsonString));
-					}else{
-						resultid = ((Department)pD.searchByFeild(pD.tablename, "name", department.getName())).getId();
-					}
-					
-					pDO.save(Integer.parseInt(id), resultid, department.getOperId());
-					
-					return Cause.getSuccess(resultid);
-				}
-			}
-			
-			if(!pD.selIsExist(department.getId())){
-				return Cause.getFailcode(DaoOrganizationService.ORGIDNOTFIND, "id", "id not find");
-			}
-			
-			pDO.save(Integer.parseInt(id), department.getId(), department.getOperId());
-			
-			return Cause.getSuccess(department.getId());
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error(e);
-		}
-		return Cause.getFailcode(3000, "", "system error");
-    }
-    
-    @DELETE
-    @Path("/{id}/departments/{idD}")
-    public String deleteDep(@PathParam("id") String id,
-    		                @PathParam("idD") String idD){
-    	
-    	try{
-    		return pDO.del(id, idD);
-    	}catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error(e);
-		}
-		return Cause.getFailcode(3000, "", "system error");
-    	
-    }
     
     @GET
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("/{id}/departments")
-    public String getDep(@PathParam("id") String id){
-    	
-    	try{
-    		
-    		return pDO.get(id);
-    		
-    	}catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error(e);
-		}
-		return Cause.getFailcode(3000, "", "system error");
-    }
-    
-    @DELETE
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("/{id}/departments/{idD}")
-    public String getOneDep(@PathParam("id") String id,
-                            @PathParam("idD") String idD){
-    	try{
-    		
-    		return pDO.get(id);
-    		
-    	}catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error(e);
-		}
-		return Cause.getFailcode(3000, "", "system error");
-    }
-    
-    @GET
-    @Path("")
     public String getAll(){
     	try{
     		
