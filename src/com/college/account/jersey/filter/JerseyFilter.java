3c20ffaa0373da9e7a40b9fe54b3d7f01e607fbb
xxx2406070412 <906702424@qq.com>
2015-05-21 00:44:41 +0800

    update
    updateupdate

--- src/com/college/account/jersey/filter/JerseyFilter.java
+++ src/com/college/account/jersey/filter/JerseyFilter.java
@@ -7,8 +7,7 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+
 import javax.ws.rs.ext.Provider;
 
 import com.sun.jersey.spi.container.ContainerRequest;
