69a67c8e9d237a99061d3c602a5b4258bcd09ea3
xxx2406070412 <906702424@qq.com>
2015-05-26 00:46:17 +0800

    update
    update

--- src/com/college/account/jersey/filter/JerseyFilter.java
+++ src/com/college/account/jersey/filter/JerseyFilter.java
@@ -1,13 +1,15 @@
 package com.college.account.jersey.filter;
 
-import java.util.Iterator;
+import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import javax.ws.rs.ext.Provider;
 
@@ -19,43 +21,48 @@ public class JerseyFilter implements ContainerRequestFilter{
 
 	@Context   
     private HttpServletRequest servletRequest;  
-    @Context  
-    private HttpServletResponse servletResponse;
 	@Override
 	public ContainerRequest filter(ContainerRequest arg0) {
 
 		StringBuffer s = servletRequest.getRequestURL();
 		
-		if(s.toString().contains("login")){
+		Map<String, javax.ws.rs.core.Cookie> cmap = arg0.getCookies();
+		
+		Set<String> key = cmap.keySet();
+		
+		String userid = null;
+		if(key.contains("id")){
 			
-			Map para = arg0.getFormParameters();
-			/* login  取出 cookie */
-			@SuppressWarnings("rawtypes")
-			Map cmap = arg0.getCookies();
-			
-			Set<String> key = cmap.keySet();
-			
-			if(key.contains("id"));
+			javax.ws.rs.core.Cookie obj = (javax.ws.rs.core.Cookie)cmap.get("id");
+			userid = obj.getValue();
+		}else{
+			Response response = Response.ok("{\"error\":{\"field\":\"cookies\",\"description\":\"lost para\",\"code\":1},\"ack\":\"failure\"}").status(401).type(MediaType.APPLICATION_JSON).build();  
+		    throw new WebApplicationException(response);
 		}
 		
-		Map smap = arg0.getCookies();
+		HttpSession session = servletRequest.getSession();
+		Date date = new Date();
+		long timestamp = date.getTime();
 		
-		
-		Cookie[] array = servletRequest.getCookies();
-		
-		Set<String> key = smap.keySet();
-        for (Iterator it = key.iterator(); it.hasNext();) {
-            String ss = (String) it.next();
-            System.out.println(ss);
-            System.out.println(smap.get(ss));
-        }
-        
-       // if(null != arg0){
-       // 	Response response = Response.ok("{99:\"error\"}").status(401).type(MediaType.APPLICATION_JSON).build();  
-       //     throw new WebApplicationException(response); // Throw new UnAuthorized  
-       // }
-        
-		//System.out.println(smap);
+		if(s.toString().contains("login")){
+			
+			
+			session.setAttribute(userid, timestamp);
+			session.setMaxInactiveInterval(60*60);
+		}else{
+			 Object obj = session.getAttribute(userid);
+			 
+			 if(null == obj){
+				 Response response = Response.ok("{\"error\":{\"field\":\"cookies\",\"description\":\"no auth\",\"code\":1},\"ack\":\"failure\"}").status(401).type(MediaType.APPLICATION_JSON).build();  
+				 throw new WebApplicationException(response);
+			 }
+			 
+			 long oldstamp = Long.parseLong(obj.toString());
+			 
+			 if((timestamp-oldstamp) > 60*30*1000){
+				 session.setMaxInactiveInterval(60*60);
+			 }
+		}
 		
 		return arg0;
 	}
